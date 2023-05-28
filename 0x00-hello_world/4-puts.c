15efb2490969b13e602b939d1f6dd741eb20bd66
bara080 <baraahmad232@gmail.com>
2023-05-28 13:12:00 -0400

    basic c tutorials

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - main entry point
+*return : always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like abuilding a multiple puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printif.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *return : always 0 (success)
+ */
+int main(void)
+{
+	printf("with propper grammer, but the outcome is a place of art,\n");
+	return (0);
+}
